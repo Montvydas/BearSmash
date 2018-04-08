136d6fd3de5aa430b438a4c8b30c7f1823a29e2d
Montvydas Klumbys <montvydas.klumbys@gmail.com>
2018-04-08 07:21:56 +0100

    Added micropython files

---
+++ bear/boot.py
@@ -0,0 +1,11 @@
+# This file is executed on every boot (including wake-boot from deepsleep)
+#import esp
+#esp.osdebug(None)
+import gc
+#import webrepl
+#webrepl.start()
+gc.collect()
+
+import wifimanager
+wifimanager.connect("MontyBestWifi", "donotconnect123")
+print("hello world")
---
+++ bear/wifimanager.py
@@ -0,0 +1,22 @@
+import network
+
+
+def connect(username, password):
+    sta_if = network.WLAN(network.STA_IF)
+    if not sta_if.isconnected():
+        print('connecting to network...')
+        sta_if.active(True)
+        sta_if.connect(username, password)
+        while not sta_if.isconnected():
+            pass
+    print('network config:', sta_if.ifconfig())
+
+
+def disconnect():
+    network.disconnect()
+    print("disconnected from the network.")
+
+
+def is_connected():
+    sta_if = network.WLAN(network.STA_IF)
+    return sta_if.isconnected()
